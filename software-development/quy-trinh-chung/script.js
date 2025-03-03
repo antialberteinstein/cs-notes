3e15944bfef3e0a45ec501c0611f787efd6845a9
Nhat Nguyen <153537649+antialberteinstein@users.noreply.github.com>
2025-03-03 15:52:39 +0700

    Update script.js

--- software-development/quy-trinh-chung/script.js
+++ software-development/quy-trinh-chung/script.js
@@ -1,7 +1,4 @@
-// JavaScript sẽ xử lý các hành động như lưu nội dung khi người dùng thay đổi
-
-// Lắng nghe sự kiện khi người dùng thay đổi nội dung
-document.querySelector('.content').addEventListener('input', function() {
+// Bạn có thể thêm các chức năng động ở đây, ví dụ như tự động lưu nội dung khi thay đổi
+document.querySelector('.post-body').addEventListener('input', function() {
   console.log('Nội dung đã thay đổi: ', this.innerHTML);
-  // Bạn có thể thêm các chức năng để lưu lại nội dung hoặc xử lý thêm ở đây
 });
